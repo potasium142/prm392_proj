23841e3f0751212eb314b8f5aeaa77cfcc0e8065
trinhquangtung <trinhquangtung3105@gmail.com>
2024-10-16 10:31:24 +0700

    First code

---
+++ app/src/main/java/com/example/prm392_proj/activities/ReviewActivity.java
@@ -0,0 +1,13 @@
+package com.example.prm392_proj.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.prm392_proj.R;
+
+public class ReviewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ativity_review);
+    }
+}
