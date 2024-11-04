32feb3eddfc25f3b9ea142506ed39ce8a2b90331
Tran Thanh Nhan <nhantt@kdigi.vn>
2024-11-05 03:00:02 +0700

    add ingredient fragment

--- app/src/main/java/com/example/prm392_proj/activity/RecipeEditableActivity.java
+++ app/src/main/java/com/example/prm392_proj/activity/RecipeEditableActivity.java
@@ -8,14 +8,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.prm392_proj.R;
+import com.example.prm392_proj.adapter.RecipeEditableViewPagerAdapter;
+import com.example.prm392_proj.frament.IngredientListEditableFragment;
 import com.example.prm392_proj.model.Recipe;
 import com.example.prm392_proj.model.User;
 import com.example.prm392_proj.repository.UserRepository;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.squareup.picasso.Picasso;
 
 public class RecipeEditableActivity extends AppCompatActivity {
@@ -41,6 +47,21 @@ public class RecipeEditableActivity extends AppCompatActivity {
         User user = userRepository.getUserById(recipe.getUserCreatorId());
         TextView userChannel = findViewById(R.id.userChannel);
         userChannel.setText(user.getProfileName());
+
+        var adapter = new RecipeEditableViewPagerAdapter(this);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        ViewPager2 viewPager = findViewById(R.id.viewPager2);
+        viewPager.setAdapter(adapter);
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("Ingredients");
+                    break;
+                case 1:
+                    tab.setText("Procedures");
+                    break;
+            }
+        }).attach();
     }
 }
 
