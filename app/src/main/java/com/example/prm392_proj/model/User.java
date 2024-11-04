2f3ecacdf1c4203f29e059f669caf56211e360bd
Tran Thanh Nhan <nhantt@kdigi.vn>
2024-11-04 15:40:57 +0700

    update model

--- app/src/main/java/com/example/prm392_proj/model/User.java
+++ app/src/main/java/com/example/prm392_proj/model/User.java
@@ -1,12 +1,10 @@
 package com.example.prm392_proj.model;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,18 +20,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
-public class User implements Parcelable {
-    public static final Creator<User> CREATOR = new Creator<User>() {
-        @Override
-        public User createFromParcel(Parcel in) {
-            return new User(in);
-        }
-
-        @Override
-        public User[] newArray(int size) {
-            return new User[size];
-        }
-    };
+public class User implements Serializable {
     @PrimaryKey(autoGenerate = true)
     private int id;
     private String username;
@@ -50,26 +37,4 @@ public class User implements Parcelable {
         this.avatar = avatar;
     }
 
-    protected User(Parcel in) {
-        id = in.readInt();
-        username = in.readString();
-        email = in.readString();
-        password = in.readString();
-        profileName = in.readString();
-        avatar = in.readString();
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(@NonNull Parcel dest, int flags) {
-        dest.writeInt(this.id);
-        dest.writeString(this.username);
-        dest.writeString(this.email);
-        dest.writeString(this.password);
-        dest.writeString(this.profileName);
-    }
 }
